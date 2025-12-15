36bda23ac19ee56b23928e228ee624c22090372a
Jaden Hampton <wearewebtastic@gmail.com>
2025-12-14 22:52:33 -0700

    Add hyperlink back to main website: Added small tweaks on backend

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,6 +4,8 @@ import Footer from "@/components/Footer";
 import Image from 'next/image';
 import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
@@ -26,6 +28,15 @@ const Layout = async ({ children }: { children: ReactNode }) => {
         <div className="w-full mx-auto max-w-7xl px-4 relative z-10 flex flex-col min-h-screen">
             <Header session = { session } />
               <div className="mt-20 pb-20 flex-1">{children}</div>
+            
+            <div className="flex justify-center pb-8">
+                <Link href="https://whitefishskijoring.org" target="_blank" rel="noopener noreferrer">
+                    <Button variant="outline" className="text-white border-white hover:bg-white hover:text-black bg-transparent/20 backdrop-blur-sm cursor-pointer">
+                        Back to Whitefish Skijoring Website
+                    </Button>
+                </Link>
+            </div>
+
             <Footer />
         </div>
     </main>
